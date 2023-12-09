74dfdd9408a856745fc662d83122b20242a78688
mshaik <mshaik@yandex-team.ru>
2023-12-09 23:47:04 +0300

    project second part fix

--- src/controllers/cards.ts
+++ src/controllers/cards.ts
@@ -29,8 +29,9 @@ export const deleteCard = async (req: Request, res: Response, next: NextFunction
   try {
     const card = await Card.findById(req.params.cardId).orFail(() => new NotFoundError('Нет карточки с таким id'));
 
-    if (card.ownerId !== req.user?._id) {
+    if (card.ownerId.toString() !== req.user?._id) {
       next(new ForbiddenError('Вы не можете удалить чужую карточку'));
+      return
     }
 
     await card.deleteOne();
